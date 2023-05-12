b2bc59cfa1dac67cfa36c4661238a536005257e4
juanmarquesdev <juanbatistamarques+github@gmail.com>
2023-05-12 10:03:35 -0300

    Game seed

--- src/main/java/com/juanmarquesdev/dslist/entities/Game.java
+++ src/main/java/com/juanmarquesdev/dslist/entities/Game.java
@@ -19,14 +19,19 @@ public class Game {
     private Integer year;
     private String genre;
     private String platforms;
+    private Double score;
     private String imgUrl;
+
+    @Column(columnDefinition = "TEXT")
     private String shortDescription;
+
+    @Column(columnDefinition = "TEXT")
     private String longDescription;
 
     public Game() {
     }
 
-    public Game(Long id, String title, Integer year, String genre, String platforms, String imgUrl,
+    public Game(Long id, String title, Integer year, String genre, String platforms, Double score, String imgUrl,
             String shortDescription,
             String longDescription) {
         this.id = id;
@@ -34,6 +39,7 @@ public class Game {
         this.year = year;
         this.genre = genre;
         this.platforms = platforms;
+        this.score = score;
         this.imgUrl = imgUrl;
         this.shortDescription = shortDescription;
         this.longDescription = longDescription;
@@ -77,6 +83,14 @@ public class Game {
 
     public void setPlatforms(String platforms) {
         this.platforms = platforms;
+    }
+
+    public Double getScore() {
+        return score;
+    }
+
+    public void setScore(Double score) {
+        this.score = score;
     }
 
     public String getImgUrl() {
