e24f4131514bd4917ad3497f17c1e32dc834a894
juanmarquesdev <juanbatistamarques+github@gmail.com>
2023-05-12 16:58:09 -0300

    Move games on lists

--- src/main/java/com/juanmarquesdev/dslist/repositories/GameListRepository.java
+++ src/main/java/com/juanmarquesdev/dslist/repositories/GameListRepository.java
@@ -1,11 +1,15 @@
 package com.juanmarquesdev.dslist.repositories;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
 
 import com.juanmarquesdev.dslist.entities.GameList;
 
 /* Used to make interface between the application and database */
 /* JpaRepository<"Entity that are mapped in database", "Type of id column of this entity"> */
 public interface GameListRepository extends JpaRepository<GameList, Long> {
-
+    @Modifying
+    @Query(nativeQuery = true, value = "UPDATE tb_belonging SET position = :newPosition WHERE list_id = :listId AND game_id = :gameId")
+    void updateBelongingPosition(Long listId, Long gameId, Integer newPosition);
 }
--- src/main/java/com/juanmarquesdev/dslist/services/GameListService.java
+++ src/main/java/com/juanmarquesdev/dslist/services/GameListService.java
@@ -7,7 +7,9 @@ import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
 import com.juanmarquesdev.dslist.repositories.GameListRepository;
+import com.juanmarquesdev.dslist.repositories.GameRepository;
 import com.juanmarquesdev.dslist.dto.GameListDTO;
+import com.juanmarquesdev.dslist.projections.GameMinProjection;
 
 /* Register the component to be used in the application*/
 @Service
@@ -17,16 +19,35 @@ public class GameListService {
     @Autowired
     private GameListRepository gameListRepository;
 
+    @Autowired
+    private GameRepository gameRepository;
+
     @Transactional(readOnly = true)
     public List<GameListDTO> findAll() {
         /* stream() -> Make operations with data sequence */
         /* map() -> Transform objects */
         return gameListRepository.findAll().stream().map(x -> new GameListDTO(x)).toList();
     }
+    
+    @Transactional
+	public void move(Long listId, int sourceIndex, int destinationIndex) {
+
+		List<GameMinProjection> list = gameRepository.searchByList(listId);
+
+		GameMinProjection obj = list.remove(sourceIndex);
+		list.add(destinationIndex, obj);
+
+		int min = sourceIndex < destinationIndex ? sourceIndex : destinationIndex;
+		int max = sourceIndex < destinationIndex ? destinationIndex : sourceIndex;
+
+		for (int i = min; i <= max; i++) {
+			gameListRepository.updateBelongingPosition(listId, list.get(i).getId(), i);
+		}
+	}
 
     // @Transactional(readOnly = true)
     // public GameListDTO findById(Long id) {
-    //     /* We need the function get() to get the object */
-    //     return new GameListDTO(gameListRepository.findById(id).get());
+    // /* We need the function get() to get the object */
+    // return new GameListDTO(gameListRepository.findById(id).get());
     // }
 }
